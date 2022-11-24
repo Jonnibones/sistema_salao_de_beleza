8aca7f492588ce5e40d596a2ea1201a171ada507
CristianoSanto <crrs008@gmail.com>
2022-11-23 21:57:59 -0300

    Alterando favicon

--- app/Views/master_view.php
+++ app/Views/master_view.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo base_url();?>/src/img/sob.png" type="image/x-icon" />
     <title><?php echo $title; ?></title>
     
 </head>
