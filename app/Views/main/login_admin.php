0a4507f1dbb208adb4c6280271772f4c50e9914b
Eduardo luiz cunha <edunoigbauer@hotmail.com>
2022-11-28 22:27:38 -0300

    minhas alteracoes

--- app/Views/main/login_admin.php
+++ app/Views/main/login_admin.php
@@ -1,8 +1,54 @@
 
-    <form action="" method="post">
-        <label for="">Login</label>
-        <input type="text" name="" id="">
-        <label for="">Senha</label>
-        <input type="password" name="" id="">
-        <button type="submit">Entrar</button>
-    </form>
+
+<style>
+       body {
+      font-family:ariel;
+        }
+
+       div {
+background-color: rgba(0, 0, 0,0.8 ) ;
+position: absolute;
+top: 50%;
+left:50%;
+transform: translate(-50%,-50%); 
+padding:80px;
+border-radius:15px;
+color: white;
+
+   }
+   input{
+    padding:15px;
+    border:none;
+    outline:none;
+    font-size:15px;
+   }
+   button{
+    background-color:dodgerblue;
+    border:none;
+    padding:15px;
+    width:100%;
+    border-radius:10px;
+    color:white;
+    font-size:15px;
+   }
+   button:hover{
+background-color:deepskyblue;
+cursor:pointer;
+   }
+    </style>
+    <div>
+
+        <h1>Login</h1>
+        <input type="text" placeholder="Nome"><br><br>
+        <input type="password"  placeholder="Senha">
+        <br><br>
+        <button>Enviar</button><br>
+
+
+        
+    
+
+
+    </div>
+
+
